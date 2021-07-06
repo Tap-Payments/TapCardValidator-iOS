84a544d1add9cd37ce9b1ce5d493597823e326c4
O Rabie <o.rabie@tap.company>
2021-07-06 11:38:51 +0200

    Added Card Brand Scheme

--- TapCardValidator/Source/CardBrand.swift
+++ TapCardValidator/Source/CardBrand.swift
@@ -4,10 +4,11 @@
 //
 //  Copyright © 2018 Tap Payments. All rights reserved.
 //
-
 /// Card Brand.
-@objc public enum CardBrand: Int {
+import Foundation
 
+@objc public enum CardBrand: Int,CaseIterable {
+    
     case aiywaLoyalty
     case americanExpress
     case benefit
@@ -33,19 +34,22 @@
     case unionPay
     case verve
     case visa
-	case visaElectron
+    case visaElectron
     case viva
     case wataniya
     case zain
-
+    case orange
+    case etisalat
+    case vodafone
+    
     case unknown
-
+    
     // MARK: - Private -
-
+    
     private struct RawValues {
-
+        
         fileprivate static let table: [CardBrand: [String]] = [
-
+            
             .aiywaLoyalty       : RawValues.aiywaLoyalty,
             .americanExpress    : RawValues.americanExpress,
             .benefit            : RawValues.benefit,
@@ -71,12 +75,15 @@
             .unionPay           : RawValues.unionPay,
             .verve              : RawValues.verve,
             .visa               : RawValues.visa,
-			.visaElectron		: RawValues.visaElectron,
+            .visaElectron       : RawValues.visaElectron,
             .viva               : RawValues.viva,
             .wataniya           : RawValues.wataniya,
-            .zain               : RawValues.zain
+            .zain               : RawValues.zain,
+            .orange             : RawValues.orange,
+            .etisalat           : RawValues.etisalat,
+            .vodafone           : RawValues.vodafone
         ]
-
+        
         private static let aiywaLoyalty     = ["Aiywa Loyalty"]
         private static let americanExpress  = ["AMERICAN_EXPRESS", "AMEX"]
         private static let benefit          = ["BENEFIT"]
@@ -95,32 +102,47 @@
         private static let masterCard       = ["MASTERCARD"]
         private static let naps             = ["NAPS"]
         private static let nspkMir          = ["NSPK"]
-        private static let omanNet          = ["OMAN_NET"]
+        private static let omanNet          = ["OMAN_NET","OMANNET"]
         private static let sadad            = ["SADAD_ACCOUNT"]
         private static let tap              = ["TAP"]
         private static let uatp             = ["UATP"]
         private static let unionPay         = ["UNION_PAY", "UNIONPAY"]
         private static let verve            = ["VERVE"]
         private static let visa             = ["VISA"]
-		private static let visaElectron		= ["VISA_ELECTRON"]
+        private static let visaElectron     = ["VISA_ELECTRON"]
         private static let viva             = ["Viva PAY"]
         private static let wataniya         = ["Wataniya PAY"]
         private static let zain             = ["Zain PAY"]
-
+        private static let orange           = ["ORANGE PAY"]
+        private static let etisalat         = ["ETISALAT PAY"]
+        private static let vodafone         = ["VODAFONE PAY"]
+        
         @available(*, unavailable) private init() {}
+    }
+    
+    /// Represents which type of brands is this cards or telecom or apple pay
+    public var brandSegmentIdentifier:String {
+        get {
+            switch self {
+            case .zain,.viva,.wataniya,.vodafone,.etisalat,.orange:
+                return "telecom"
+            default:
+                return "cards"
+            }
+        }
     }
 }
 
 // MARK: - Encodable
 extension CardBrand: Encodable {
-
+    
     public func encode(to encoder: Encoder) throws {
-
+        
         guard let value = RawValues.table[self]?.first else {
-
+            
             fatalError("Unknown card brand.")
         }
-
+        
         var container = encoder.singleValueContainer()
         try container.encode(value)
     }
@@ -128,20 +150,20 @@ extension CardBrand: Encodable {
 
 // MARK: - Decodable
 extension CardBrand: Decodable {
-
+    
     public init(from decoder: Decoder) throws {
-
+        
         let container = try decoder.singleValueContainer()
         let value = try container.decode(String.self)
-
+        
         for (brand, rawValues) in RawValues.table {
-
+            
             guard rawValues.contains(value) else { continue }
-
+            
             self = brand
             return
         }
-
+        
         self = .unknown
     }
 }
